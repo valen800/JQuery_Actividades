9dc9be030ebda8cb355b5f08dd999e253e212079
Valentin Ruiz Lopez <varulo.cvcv@gmail.com>
2021-01-13 11:27:52 +0100

    Exercises completed

--- Practica_1/Ejercicio_4(Hecho)/codigo.js
+++ Practica_1/Ejercicio_4(Hecho)/codigo.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
-    var firstInput = parseInt($("#inputNumber1Id").val());
-    var SecondInput = parseInt($("#inputNumber2Id").val());
 
-    $("input[type=checkbox]").click(function() {
-        /* if ($("input[type=checkbox]:checked")) */
-        checkForm(firstInput, SecondInput);
+    $("input[type=checkbox]").change(function(event) {
+        var firstInput = parseInt($("#inputNumber1Id").val());
+        var SecondInput = parseInt($("#inputNumber2Id").val());
+
+        if ($(this).is(':checked')) {
+            checkForm(firstInput, SecondInput, event);
+        } else {
+            $('#inputResultId').remove();
+        }
     })
 });
 
-function checkForm(firstInput, SecondInput) {
+function checkForm(firstInput, SecondInput, event) {
+    
     if (firstInput != "" || SecondInput != "") {
         if (!isNaN(firstInput) || !isNan(SecondInput)) {
             let result = firstInput + SecondInput;
+            console.log(result);
 
             let input = $("<input/>", {
                 type: 'text',
                 id: 'inputResultId',
                 value: result,
+                disabled: true,
             });
-            
+
+            /* input.prop('disabled', true); */
+                    
             $("#myFormId").append(input);
         } else {
             if (event.preventDefault) {
