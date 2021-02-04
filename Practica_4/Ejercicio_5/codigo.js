e01f6a9ffaaadffdd2c3ff39b04711364e87c8d6
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2021-02-04 21:24:20 +0100

    Practica 4 completada

--- Practica_4/Ejercicio_5/codigo.js
+++ Practica_4/Ejercicio_5/codigo.js
@@ -3,14 +3,19 @@ $(document).ready(function() {
 
     $("#initEfect").click(function() {
         var cube = $("#cube");
-        Effectqueue().queue(Effectqueue());
+        Effectqueue(cube);
     })
 })
 
 function Effectqueue(cube) {
 
     cube.animate({height: "300"}, 1000)
-    cube.css("background-color", "blue")
+    cube.delay(2000)
+    cube.queue(function(cotinua) {
+        cube.css("background-color", "blue")
+        cotinua();
+    })
+    cube.delay(2000)
     cube.animate({width: "300"}, 1000)
 }
 // https://aules.edu.gva.es/fp/mod/forum/discuss.php?d=80608
