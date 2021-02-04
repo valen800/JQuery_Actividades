675122802612471441aa97a134005b295698b355
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2021-02-04 13:51:53 +0100

    Efectos

--- Practica_4/Ejercicio_4/codigo.js
+++ Practica_4/Ejercicio_4/codigo.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-    $("#initEfect").click(function() {
+    $('h1').click(function(){
         $("h1").animate({
             color: "red"
-        }, 1000)
-    })
+        }, 1500);
+    });
+    // Sirve solo para uno, que es el que pulsas.
+    /* $('h1').click(function(){
+        $(this).animate({
+            color: "red"
+        }, 1500);
+    }); */
 })
--- Practica_4/Ejercicio_5/codigo.js
+++ Practica_4/Ejercicio_5/codigo.js
@@ -1,29 +1,16 @@
-var t = 0;
-
 $(document).ready(function() {
-    $("#initEfect").click(function() {
-        moveit();
-    })
 
-    $("#stopEfect").click(function() {
-        $(".content").stop();
+
+    $("#initEfect").click(function() {
+        var cube = $("#cube");
+        Effectqueue().queue(Effectqueue());
     })
 })
 
-function moveit() {
-    t += 0.05;
+function Effectqueue(cube) {
 
-    var r = 100;         // radius
-    var xcenter = 100;   // center X position
-    var ycenter = 100;   // center Y position
-
-    var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
-    var newTop = Math.floor(ycenter + (r * Math.sin(t)));
-
-    $(".content").animate({
-        top: newTop,
-        left: newLeft,    
-    }, 1, function() {
-        moveit()
-    })
+    cube.animate({height: "300"}, 1000)
+    cube.css("background-color", "blue")
+    cube.animate({width: "300"}, 1000)
 }
+// https://aules.edu.gva.es/fp/mod/forum/discuss.php?d=80608
